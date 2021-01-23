a2bdfa914fa7b33bc4478433e1041900cc351383
Alan Luo <i@iltc.io>
2021-01-22 22:56:46 -0500

    Initial Commit

---
+++ Shared/TipCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorApp.swift
+//  Shared
+//
+//  Created by Alan Luo on 1/22/21.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
